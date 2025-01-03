f82217007932298d797828262695de516dd5ea2e
abbasamirza <76699564+abbasamirza@users.noreply.github.com>
2025-01-03 16:28:49 +0500

    Create card for login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -4,6 +4,7 @@ import Brand from "./Brand";
 import Footer from "./Footer";
 import AuthContext from "../context/authentication/authContext";
 import { useNavigate } from "react-router-dom";
+import Card from "./Card";
 
 const Login = () => {
   const authContext = useContext(AuthContext);
@@ -38,31 +39,43 @@ const Login = () => {
       <main className="main-height">
         <form onSubmit={handleLoginSubmit}>
           <div className="container-main-form">
-            <label className="label" htmlFor="email">
-              Email
-            </label>
-            <input
-              className="input"
-              type="email"
-              placeholder="Enter Email"
-              name="email"
-              required
-              autoComplete="username"
-            />
-            <label className="label" htmlFor="password">
-              Password
-            </label>
-            <input
-              className="input"
-              type="password"
-              placeholder="Enter Password"
-              name="password"
-              required
-              autoComplete="current-password"
-            />
-            <button className="btn" type="submit">
-              Login
-            </button>
+            <Card
+              style={{ width: "40%", height: "max-content", padding: "3rem" }}
+            >
+              <div>
+                <label className="label" htmlFor="email">
+                  Email
+                </label>
+                <input
+                  className="input"
+                  type="email"
+                  placeholder="Enter Email"
+                  name="email"
+                  required
+                  autoComplete="username"
+                />
+              </div>
+              <div>
+                <label className="label" htmlFor="password">
+                  Password
+                </label>
+                <input
+                  className="input"
+                  type="password"
+                  placeholder="Enter Password"
+                  name="password"
+                  required
+                  autoComplete="current-password"
+                />
+              </div>
+              <button
+                className="btn"
+                type="submit"
+                style={{ marginTop: "20px" }}
+              >
+                Login
+              </button>
+            </Card>
           </div>
         </form>
       </main>
