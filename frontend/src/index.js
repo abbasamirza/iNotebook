79baf0c218d65404f1c46e48f99e2780e221d8da
abbasamirza <76699564+abbasamirza@users.noreply.github.com>
2025-01-18 11:43:53 +0500

    Initialized new project and added tailwind css

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <div className="bg-red-500 h-screen w-screen flex justify-center items-center">
+      Testing
+    </div>
+  </React.StrictMode>
+);
