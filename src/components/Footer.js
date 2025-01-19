0f3012bd09bc3839ede12f620c043b68319295da
abbasamirza <76699564+abbasamirza@users.noreply.github.com>
2025-01-19 11:27:19 +0500

    Delete old files

--- src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-import "../styles/css/Footer.css";
-import React from "react";
-
-const Footer = () => {
-  return <p className="footer-text">Copyright &copy; All Rights Reserved.</p>;
-};
-
-export default Footer;
--- src/context/sidenav/sidenavContext.js
+++
@@ -1,5 +0,0 @@
-import { createContext } from "react";
-
-const SidenavContext = createContext();
-
-export default SidenavContext;
